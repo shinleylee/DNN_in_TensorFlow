68d8f9e932366e49f58c0a2d901d55fc06c02110
shinleylee <luxi__li@sina.com>
2017-11-13 15:35:19 +0800

    continue learning Keras

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # DNN_in_TensorFlow
 some demo and test code of DNN in TensorFlow
+
+Keras的BibTex引用
+
+    @misc{chollet2015keras,
+        author = {Chollet, François and others},
+        title = {Keras},
+        year = {2015},
+        publisher = {GitHub},
+        journal = {GitHub repository},
+        howpublished = {\url{https://github.com/fchollet/keras}}
+    }
--- testKeras.py
+++ testKeras.py
@@ -2,21 +2,34 @@ import keras
 
 # state a model
 model = keras.models.Sequential()
-
-# draw the graph
+#1 draw the graph
 model.add(keras.layers.Dense(units=64, input_dim=100))
 model.add(keras.layers.Activation("relu"))
 model.add(keras.layers.Dense(units=10))
 model.add(keras.layers.Activation("softmax"))
 
-# compile
+#2 compile
 model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['Accuracy'])
 # model.compile(loss='categorical_crossentropy',optimizer=keras.optimizers.SGD(lr=0.01,momentum=0.9,nesterov=True))
 
-# train/fit
-model.fit(x_train, y_train, epochs=5, batch_size=32)
+#3 train/fit
+hist = model.fit(x_train, y_train, epochs=5, batch_size=32, validation_split=0.2)
+# validation_split : split last 20% input as validation set
+# note that the input should be shuffled manually before, because keras will process validation_split before auto_shuffle
 # model.train_on_batch(x_batch, y_batch)
+print(hist.history) # hist.history includes the loss and other metrics after each epoch
 
-# predict on test
+#4 predict on test
 loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
 classes = model.predict(x_test, batch_size=128)
+
+
+
+
+
+
+
+# save and load the Keras model (in HDF5 format)
+model.save(filepath)
+del model # deletes the existing model
+model = keras.models.load_model(filepath)
